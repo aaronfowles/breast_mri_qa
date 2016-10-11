246d08d78ee708532a410917f30f47feb8dd0d39
Aaron Fowles <aaronfowles@gmail.com>
2016-10-11 16:18:39 +0100

    0.0.11

--- breast_mri_qa_measure/fetch.py
+++ breast_mri_qa_measure/fetch.py
@@ -47,13 +47,13 @@ class Fetcher:
             dcmdata = part.get_payload(decode=True)
             if dcmdata is not None:
                 dcmobjs.append(dicom.read_file(io.BytesIO(dcmdata)))
-        ret_dcmobjs = []
-        for instance in dcmobjs:
-            if not instance:
-                continue
-            try:
-                instance[0][0x0008,0x0008] # Only images should have image type header tag
-                ret_dcmobjs.append(instance)
-            except:
-                continue
+        ret_dcmobjs = dcmobjs
+        #for instance in dcmobjs:
+        #    if not instance:
+        #        continue
+        #    try:
+        #        instance[0][0x0008,0x0008] # Only images should have image type header tag
+        #        ret_dcmobjs.append(instance)
+        #    except:
+        #        continue
         return ret_dcmobjs
