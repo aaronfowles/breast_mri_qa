b2dde2ba1907ece00e8ae7fcf2e4e750a13dbf49
Aaron Fowles <aaronfowles@gmail.com>
2016-10-25 09:48:16 +0100

    tidying up package for pypi

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'breast_mri_qa',
   packages = ['breast_mri_qa'], # this must be the same as the name above
-  version = '0.0.1',
+  version = '0.0.2',
   description = 'Functions for MRI breast screening QA image fetch and analysis',
   author = 'Aaron Fowles',
   author_email = 'aaronfowles@gmail.com',
   url = 'https://github.com/aaronfowles/breast_mri_qa', # use the URL to the github repo
-  download_url = 'https://github.com/aaronfowles/breast_mri_qa_measure/tarball/0.0.1', # I'll explain this in a second
+  download_url = 'https://github.com/aaronfowles/breast_mri_qa/tarball/0.0.2', # I'll explain this in a second
   keywords = ['aaronfowles'], # arbitrary keywords
   classifiers = [],
 )
