cb25895a09c48da351a04a5cae2bb6b2d9f1d8dd
Aaron Fowles <aaronfowles@gmail.com>
2016-10-31 14:09:29 +0000

    made Protocol objects more configurable

--- breast_mri_qa/organise.py
+++ breast_mri_qa/organise.py
@@ -1,27 +1,7 @@
 class Protocol:
 
-    def __init__(self):
-        self.required_images = [('snr_acquisition_one', self.is_snr, self.save_snr, 'TEST'),
-                                ('snr_acquisition_two', self.is_snr, self.save_snr, 'TEST'),
-                                ('spir_water', self.is_spir_water_fse, self.save_spir_water_fse, 'SPIR WATER'),
-                                ('spir_fat', self.is_spir_fat_fse, self.save_spir_fat_fse, 'SPIR FAT'),
-                                ('spair_water', self.is_spair_water_fse, self.save_spair_water_fse, 'SPAIR WATER'),
-                                ('spair_fat', self.is_spair_fat_fse, self.save_spair_fat_fse, 'SPAIR FAT'),
-                                ('coil_one_acquisition_one', self.is_coil_one, self.save_coil_one, 'COIL 1'),
-                                ('coil_two_acquisition_two', self.is_coil_one, self.save_coil_one, 'COIL 1'),
-                                ('coil_one_acquisition_two', self.is_coil_two, self.save_coil_one, 'COIL 2'),
-                                ('coil_two_acquisition_one', self.is_coil_two, self.save_coil_one, 'COIL 2'),
-                                ('coil_three_acquisition_one', self.is_coil_three, self.save_coil_one, 'COIL 3'),
-                                ('coil_three_acquisition_two', self.is_coil_three, self.save_coil_one, 'COIL 3'),
-                                ('coil_four_acquisition_one', self.is_coil_four, self.save_coil_one, 'COIL 4'),
-                                ('coil_four_acquisition_two', self.is_coil_four, self.save_coil_one, 'COIL 4'),
-                                ('coil_five_acquisition_one', self.is_coil_five, self.save_coil_one, 'COIL 5'),
-                                ('coil_five_acquisition_two', self.is_coil_five, self.save_coil_one, 'COIL 5'),
-                                ('coil_six_acquisition_one', self.is_coil_six, self.save_coil_one, 'COIL 6'),
-                                ('coil_six_acquisition_two', self.is_coil_six, self.save_coil_one, 'COIL 6'),
-                                ('coil_seven_acquisition_one', self.is_coil_seven, self.save_coil_one, 'COIL 7'),
-                                ('coil_seven_acquisition_two', self.is_coil_seven, self.save_coil_one, 'COIL 7'),
-        ]
+    def __init__(self, rules):
+        self.required_images = rules
         self.dict_protocol_instances = {}
         for obj in self.required_images:
             self.dict_protocol_instances[obj[0]] = None
@@ -75,47 +55,13 @@ class Protocol:
         else:
             return False
 
-    def save_snr(self, img_name, instance):
-        return self.save_instance(img_name, instance)
-
-    def save_spir_water_fse(self, img_name, instance):
-        return self.save_instance(img_name, instance)
-
-    def save_spir_fat_fse(self, img_name, instance):
-        return self.save_instance(img_name, instance)
-
-    def save_spair_water_fse(self, img_name, instance):
-        return self.save_instance(img_name, instance)
-
-    def save_spair_fat_fse(self, img_name, instance):
-        return self.save_instance(img_name, instance)
-
-    def save_coil_one(self, img_name, instance):
-        return self.save_instance(img_name, instance)
-
-    def save_coil_two(self, img_name, instance):
-        return self.save_instance(img_name, instance)
-
-    def save_coil_three(self, img_name, instance):
-        return self.save_instance(img_name, instance)
-
-    def save_coil_four(self, img_name, instance):
-        return self.save_instance(img_name, instance)
-
-    def save_coil_five(self, img_name, instance):
-        return self.save_instance(img_name, instance)
-
-    def save_coil_six(self, img_name, instance):
-        return self.save_instance(img_name, instance)
-
-    def save_coil_seven(self, img_name, instance):
-        return self.save_instance(img_name, instance)
 
     def assign_instances_to_protocol(self, list_instances):
         for instance in list_instances:
-            for img_name, match_func, apply_func, search_term in self.required_images:
+            for img_name, match_func, search_term in self.required_images:
+                match_func = eval('self.' + match_func)
                 if match_func(search_term, instance):
-                    if apply_func(img_name, instance):
+                    if self.save_instance(img_name, instance):
                         break
 
         missing_acquisitions = []
