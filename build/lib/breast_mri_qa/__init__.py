bc27daa4269e997a58b5407d22b1001ebea1d290
Aaron Fowles <aaronfowles@gmail.com>
2016-11-14 14:25:09 +0000

    trying to fix documentation build

--- build/lib/breast_mri_qa/__init__.py
+++
@@ -1,3 +0,0 @@
-from .measure import calc_fse, calc_snr
-from .fetch import Fetcher
-from .organise import Protocol
--- docs/_build/_sources/modules.rst.txt
+++
@@ -1,8 +0,0 @@
-breast_mri_qa
-=============
-
-.. toctree::
-   :maxdepth: 4
-
-   breast_mri_qa
-   setup
